82dfeb9ede0000cc6043e4d35abcb35c6b35bb0c
vpaniush <volodymyr2507@gmail.com>
2019-11-18 21:37:24 +0200

    adds initial packages

---
+++ Model/Entities/Note.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyNotes
+{
+    public class Note : INotifyPropertyChanged
+    {
+        string title;
+        string description;
+        string timeModified;
+
+        public int NoteId { get; set; }
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+        public string TimeModified
+        {
+            get { return timeModified; }
+            set
+            {
+                timeModified = value;
+                OnPropertyChanged("TimeModified");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Model/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace MyNotes
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }    
+}
