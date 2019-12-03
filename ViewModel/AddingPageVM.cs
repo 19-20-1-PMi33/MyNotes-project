f96f0aae79436338de15eb3b7f4c0781867dab4c
CarolineAnastasia <carolinestashkevych@gmail.com>
2019-12-03 20:27:06 +0200

    adds files for AddingPage

---
+++ ViewModel/AddingPageVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Data.Entity;
+using System.Collections.Generic;
+
+namespace MyNotes
+{
+    public class AddingPageVM 
+    {
+        
+    }
+}
