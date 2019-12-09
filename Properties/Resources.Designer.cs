17e4ec56f539ddd3a992c45cbb4d9d92acb5da57
youngvictormonet <victorstrelnikov91@gmail.com>
2019-12-09 20:51:19 +0300

    Image Loading and Logo

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace MyNotes.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logo {
+            get {
+                object obj = ResourceManager.GetObject("logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to .
         /// </summary>
         internal static string String1 {
