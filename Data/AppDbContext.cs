33922b50b591758bec1c70a611a45095871d6e7b
vpaniush <volodymyr2507@gmail.com>
2019-12-11 09:41:46 +0200

    adds needed files

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace MyNotes
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection")
+        {
+        }
+    }
+}
